e998eea2160f1df0876b843ea8fa16dde27f971a
XVenom--7 <tacnt017@protonmail.com>
2019-10-03 01:14:05 +0530

    added templates and static folders

--- website/coreapp/forms.py
+++ website/coreapp/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class Registration_form(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
+    message = forms.TextField()
+    url = forms.CharField(max_length=100)
+#this should be used to store the data from register template
